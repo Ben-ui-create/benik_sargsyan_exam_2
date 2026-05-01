1fae45119039586283762e9ea62201b4863b6e5e
Benik Sargsyan <beniksargsyan349@gmail.com>
2026-05-01 19:53:04 +0400

    added middlewares/errorHandlers.js logic

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,14 @@
+import HttpErrors from 'https-errors';
+
+export default {
+  notFound(req, res, next) {
+    next(HttpErrors(404));
+  },
+
+  errors(err, req, res, next) {
+    res.status(err.status || 500).json({
+      message: err.message,
+      errors: err.errors ? err.errors : {},
+    });
+  },
+}
