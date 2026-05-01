56f099cc0c13dd5aab7b33e20c526442e28e7bcd
Benik Sargsyan <beniksargsyan349@gmail.com>
2026-05-01 21:36:57 +0400

    end works

--- middlewares/schemas/books.schema.js
+++ middlewares/schemas/books.schema.js
@@ -1,49 +1,10 @@
-// import Joi from 'joi';
-//
-// export default {
-//   create: Joi.object({
-//     title: Joi.string().required(),
-//     author: Joi.string().required(),
-//     year: Joi.number().min(1).max(9999).required(),
-//     genre: Joi.string().valid(
-//       "fiction",
-//       "non-fiction",
-//       "science",
-//       "history",
-//       "fantasy",
-//       "mystery",
-//       "biography",
-//       "other"
-//     ).required()
-//   }),
-//
-//   update: Joi.object({
-//     title: Joi.string().required(),
-//     author: Joi.string().required(),
-//     year: Joi.number().min(1).max(9999).required(),
-//     genre: Joi.string().valid(
-//       "fiction",
-//       "non-fiction",
-//       "science",
-//       "history",
-//       "fantasy",
-//       "mystery",
-//       "biography",
-//       "other"
-//     ).required()
-//   }),
-// }
-//
-
 import Joi from 'joi';
-
-const currentYear = new Date().getFullYear();
 
 export default {
   create: Joi.object({
-    title: Joi.string().min(1).max(200).required(),
-    author: Joi.string().min(1).max(100).required(),
-    year: Joi.number().integer().min(1000).max(currentYear).required(),
+    title: Joi.string().required(),
+    author: Joi.string().required(),
+    year: Joi.number().min(1).max(9999).required(),
     genre: Joi.string().valid(
       "fiction",
       "non-fiction",
@@ -53,13 +14,13 @@ export default {
       "mystery",
       "biography",
       "other"
-    ).required(),
+    ).required()
   }),
 
   update: Joi.object({
-    title: Joi.string().min(1).max(200).required(),
-    author: Joi.string().min(1).max(100).required(),
-    year: Joi.number().integer().min(1000).max(currentYear).required(),
+    title: Joi.string().required(),
+    author: Joi.string().required(),
+    year: Joi.number().min(1).max(9999).required(),
     genre: Joi.string().valid(
       "fiction",
       "non-fiction",
@@ -69,6 +30,7 @@ export default {
       "mystery",
       "biography",
       "other"
-    ).required(),
+    ).required()
   }),
-};
+}
+
