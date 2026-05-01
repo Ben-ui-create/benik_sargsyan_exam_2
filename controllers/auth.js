c02545c8f49f60e1f6cfc01c9aada50d5085c9ad
Benik Sargsyan <beniksargsyan349@gmail.com>
2026-05-01 20:24:37 +0400

    added controllers/auth.js logic

---
+++ controllers/auth.js
@@ -0,0 +1,63 @@
+import HttpErrors from 'http-errors';
+import User from '../models/users.js';
+
+export default {
+  async login (req, res, next) {
+    try {
+      const { email, password } = req.body;
+
+      const user = await User.findByEmail(email);
+
+      if (!user || (user.password !== User.hashPassword(password))) {
+        throw new HttpErrors(401, {
+          errors: {
+            email: 'Invalid email or password',
+          },
+        });
+      }
+
+      const token = User.encrypt({
+        userId: user.id,
+      });
+
+      delete user.password;
+
+      res.json({
+        token,
+        user,
+      });
+    } catch (e) {
+      next(e);
+    }
+  },
+
+  async register (req, res, next) {
+    try {
+      const { name, age, email, password } = req.body;
+
+      if (await User.checkEmail(email)) {
+        throw new HttpErrors(422, {
+          errors: {
+            email: 'Email is already in use',
+          },
+        });
+      }
+
+      const newUser = await User.createUser({
+        name,
+        age,
+        password: User.hashPassword(password),
+        email,
+      });
+
+      delete newUser.password;
+
+      res.json({
+        message: 'User registered successfully.',
+        newUser,
+      });
+    } catch (e) {
+      next(e);
+    }
+  }
+}
