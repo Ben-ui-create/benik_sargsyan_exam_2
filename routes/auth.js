3719a61691675659788e61a52ae8c074d05f524f
Benik Sargsyan <beniksargsyan349@gmail.com>
2026-05-01 20:55:18 +0400

    added routes/auth.js logic

---
+++ routes/auth.js
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import * as controller from '../controllers/auth.js';
+import authorization from '../middlewares/authorization.js';
+import validator from '../middlewares/validation.js';
+import schema from '../middlewares/schemas/auth.schema.js';
+
+const router = Router();
+
+router.post(
+  '/register',
+  validator(schema.register, 'body'),
+  controller.register
+);
+
+router.post(
+  '/login',
+  validator(schema.login, 'body'),
+  controller.login
+);
+
+router.get(
+  '/profile',
+  authorization,
+  controller.profile
+);
+
+export default router;
